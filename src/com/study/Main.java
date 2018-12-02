e99131ded6915486562bfbe656075c58634acacc
xjWen <2899807359@qq.com>
2018-12-02 22:10:14 +0800

    2018/12/2

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -1,5 +1,8 @@
 package com.study;
 
+import com.study.method.AboutListNode;
+import com.study.structure.ListNode;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,6 +14,17 @@ public class Main {
 //        twoSum.TwoSum(nums,16);
 
 //        Hanoi.TowersOfHanoi(5,'A','C','B');
+        ListNode listNode1 = new ListNode(4);
+        ListNode listNode2 = new ListNode(15);
+        ListNode listNode3 = new ListNode(7);
+        ListNode listNode4 = new ListNode(40);
+        ListNode listNode5 = new ListNode(100);
+        ListNode listNode6 = new ListNode(0 );
+        listNode1.setNext(listNode2);
+        listNode2.setNext(listNode3);
+        listNode3.setNext(listNode4);
 
+        AboutListNode aboutListNode = new AboutListNode();
+        aboutListNode.insertListNode(listNode1,listNode5,3);
     }
 }
--- src/com/study/method/AboutListNode.java
+++ src/com/study/method/AboutListNode.java
@@ -13,4 +13,62 @@ public class AboutListNode {
         }
         return length;
     }
+    /**
+     * 插入链表节点
+     * **/
+    public ListNode insertListNode(ListNode headNode,ListNode inserNode,int position){
+        if (headNode == null){
+            return inserNode;
+        }
+        int size = getListLength(headNode);
+        if (position>size||position<1){
+            System.out.println("插入点position不在范围内");
+            return headNode;
+        }
+        if (position == 1){
+            headNode = inserNode;
+            return headNode;
+        }else {
+            ListNode previousNode = headNode;
+            int count = 1;
+            while (count<position-1){
+                previousNode = previousNode.getNext();
+                System.out.println(count+" Node data is "+previousNode.getData());
+                count++;
+            }
+            ListNode currentNode = previousNode.getNext();
+            inserNode.setNext(currentNode);
+            previousNode.setNext(inserNode);
+        }
+        return headNode;
+    }
+
+    public ListNode DeleteListNode(ListNode headNode,int position){
+        if (headNode==null){
+            System.out.println("当前链表为空，无法删除");
+            return headNode;
+        }
+        int size = getListLength(headNode);
+        if (position>size||position<1){
+            System.out.println("插入点position不在范围内");
+            return headNode;
+        }
+        if (position == 1){
+            ListNode currentNode = headNode.getNext();
+            headNode = null;
+            return currentNode;
+        }else {
+            ListNode previousNode = headNode;
+            int count = 1;
+            while (count < position){
+                previousNode = previousNode.getNext();
+                System.out.println(count+" Node data is "+previousNode.getData());
+                count++;
+            }
+            ListNode currentNode = previousNode.getNext();
+            previousNode.setNext(currentNode.getNext());
+            currentNode=null;
+        }
+        return headNode;
+    }
 }
