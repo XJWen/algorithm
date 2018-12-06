65dd18b628ccc96c67e6ea0459e6974b1fe17769
xjWen <2899807359@qq.com>
2018-12-06 21:36:10 +0800

    2018/12/6

--- src/com/study/method/Search.java
+++ src/com/study/method/Search.java
@@ -25,4 +25,32 @@ public class Search {
         }
         return -1;
     }
+
+    /**二分查找**/
+    /*迭代*/
+    public int BinarySerachIterative(int[] A,int n,int data){
+        int low =0,high = n-1,mid;
+        while (low<high){
+            mid = low + (high-low)/2;
+            if (A[mid]==data){
+                return mid;
+            } else if (A[mid]<data) {
+                low = mid+1;
+            }else {
+                high = mid-1;
+            }
+        }
+        return  -1;
+    }
+    /*递归*/
+    public int BinarySerachRescursive(int[] A,int high,int low,int data){
+        int mid = low + (high-low)/2;
+        if (A[mid]==data){
+            return mid;
+        }else if (A[mid]<data){
+            return BinarySerachRescursive(A,high,mid+1,data);
+        }else {
+            return BinarySerachRescursive(A,mid-1,low,data);
+        }
+    }
 }
