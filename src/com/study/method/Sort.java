759551a0e908cbcfaed3068a76777c128d58acf1
xjWen <2899807359@qq.com>
2018-12-06 20:54:43 +0800

    2018/12/6

--- src/com/study/method/Sort.java
+++ src/com/study/method/Sort.java
@@ -45,4 +45,94 @@ public class Sort {
             }
         }
     }
+
+    /*插入排序*/
+    public void InsertionSort(int[] A,int n){
+        //通过三个变量来做坐标
+        int i =0,j = 0,v = 0;
+        for (i=2;i<=n-1;i++){
+            //暂存A[i]值
+            v = A[i];
+            j = i;
+        }
+        while (A[j-1] > v&&j>=1){
+            A[j] = A[j-1];
+            j--;
+        }
+        A[j] = v;
+    }
+
+    /*归并排序*/
+    public void Mergesort(int[] A,int[] temp,int left,int right){
+        int mid = 0;
+        if (left>right){
+            mid = (right+left)/2;
+            Mergesort(A,temp,left,mid);
+            Mergesort(A,temp,mid+1,right);
+            Merge(A,temp,left,right,mid+1);
+        }
+    }
+    public void Merge(int[] A,int[] temp,int left,int right,int mid){
+        int i =0,left_end = 0,size = 0,temp_pos =0;
+        left_end = mid -1;
+        temp_pos = left;
+        size = right - left + 1;
+        while ((left<=left_end)&&(mid<=right)){
+            if (A[left]<=A[mid]){
+                temp[temp_pos] = A[left];
+                temp_pos = temp_pos+1;
+                left = left+1;
+            }else {
+                temp[temp_pos] = A[mid];
+                temp_pos = temp_pos+1;
+                mid = mid+1;
+            }
+        }
+        while (left<=left_end){
+            temp[temp_pos] = A[left];
+            left = left+1;
+            temp_pos +=1;
+        }
+        while (mid<=right){
+            temp[temp_pos] = A[mid];
+            mid = mid+1;
+            temp_pos = temp_pos+1;
+        }
+        for (i=0;i<=size;i++){
+            A[right] = temp[right];
+            right --;
+        }
+    }
+
+    /*快速排序*/
+    public void Quicksort(int[] A,int low,int high){
+        int pivot = 0;
+        if (high>low){
+            pivot = Partition(A,low,high);
+            Quicksort(A,low,pivot-1);
+            Quicksort(A,pivot+1,high);
+        }
+    }
+    public int Partition(int[] A,int low,int high){
+        int left,right,pivot_item = A[low];
+        left = low;
+        right = high;
+        while (left<right){
+            /*当item<pivot移动左指针*/
+            while (A[left]<=pivot_item){
+                left++;
+            }
+            /*当item>pivot移动右指针*/
+            while (A[right]>=pivot_item){
+                right++;
+            }
+            if (left<right){
+
+            }
+
+        }
+        A[low] = A[right];
+        A[right] = pivot_item;
+        return right;
+    }
 }
