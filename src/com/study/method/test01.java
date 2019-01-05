d4dfb0e9839851035b795f10420f93c7a3de7e9e
xjWen <2899807359@qq.com>
2019-01-05 22:27:07 +0800

    2019/1/5

--- src/com/study/method/test01.java
+++ src/com/study/method/test01.java
@@ -3,18 +3,20 @@ package com.study.method;
 public class test01 {
 
     /**
-     * 斐波那契数列
+     * 古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
+     * 斐波那契数列 指数爆炸
      * **/
     public static void main(String[] args){
+        //f1代表父代，f2代表子代，1代表一对兔子
         int f1=1,f2=1,f;
         int M=30;
         System.out.println(f1);
-        System.out.println(f1);
+        System.out.println(f2);
         for (int i=3;i<M;i++){
             f=f2;
-            f1=f1+f2;
+            f2=f1+f2;
             f1=f;
-            System.out.println(f1);
+            System.out.println(f2);
         }
     }
 }
---
+++ src/com/study/method/test02.java
@@ -0,0 +1,27 @@
+package com.study.method;
+
+public class test02 {
+
+    /**
+     * 判断101-200之间有多少个素数，并输出所有素数。
+     * 程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，如果能被整除， 则表明此数不是素数，反之是素数。
+     * **/
+    public static void main(String[] args){
+        int count =0;
+        //+=2是因为10+偶数一定非素数
+        for (int i=101;i<200;i+=2){
+            boolean flag =true;
+            for (int j=2;j<=Math.sqrt(i);j++){
+                if (i%j==0){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag==true){
+                count++;
+                System.out.println(i+"是素数");
+            }
+        }
+        System.out.println("素数个数总共有"+count);
+    }
+}
