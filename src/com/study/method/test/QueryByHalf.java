183ffb0038a046618135ad37a769bf18681262f6
xjWen <2899807359@qq.com>
2019-03-18 10:05:25 +0800

    2019/3/18

---
+++ src/com/study/method/test/QueryByHalf.java
@@ -0,0 +1,34 @@
+package com.study.method.test;
+
+/**
+ * 二分法查找相关元素
+ * **/
+public class QueryByHalf {
+
+    public static void main(String[] args){
+        String str[] = {"a","b","c","d","e","f","g","h"};
+        int bsearch = bsearch(str, "g");
+        System.out.println(bsearch);
+
+    }
+
+    /**
+     * m 的位置在整个数组里比要查的字符串v  <  l=m+1
+     *   >  r=m-1
+     * **/
+    static int bsearch(String[] str,String v){
+        int l =0,r=str.length-1;
+        while (l<=r){
+            int m = (l+r)/2;
+            if (str[m].compareTo(v)==0){
+                return m;
+            }else if (str[m].compareTo(v)<0){
+                l = m+1;
+            }else if (str[m].compareTo(v)>0){
+                r = m-1;
+            }
+        }
+        return -1;
+    }
+}
+
---
+++ src/com/study/method/test/QuickSort.java
@@ -0,0 +1,39 @@
+package com.study.method.test;
+
+public class QuickSort {
+
+    public static int Partition(int a[],int p,int r){
+        int x = a[r-1];//取最末尾的数
+        int i = p-1;//从0开始
+        int temp;
+        for (int j=p;j<=r-1;j++){
+            if (a[j-1]<=x){
+                i++;//用来判断数组前半段的有序度指针
+                temp=a[j-1];
+                a[j-1]=a[i-1];
+                a[i-1]=temp;
+            }
+        }
+        temp=a[r-1];
+        a[r-1]=a[i+1-1];//前i项已排列，将第i项放到最后一位做参考变量
+        a[i+1-1]=temp;
+        return i+1;
+    }
+
+    public static void QuickSort(int a[],int p,int r){
+        if (p<r){
+            int q = Partition(a,p,r);
+            System.out.println(q);
+            QuickSort(a,p,q-1);
+            QuickSort(a,q+1,r);
+        }
+    }
+
+    public static void main(String[] args){
+        int a[]={23,53,77,36,84,76,93,13,45,23};
+        QuickSort(a,1,10);
+        for (int i=1;i<=10;i++){ System.out.println(a[i-1]);}
+
+
+    }
+}
