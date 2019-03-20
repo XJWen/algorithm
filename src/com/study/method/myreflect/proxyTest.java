a32480590d6f8081f71d2f99d3d77a0549afb287
xjWen <2899807359@qq.com>
2019-03-20 14:57:46 +0800

    2019/3/20 动态代理 反射实现AOP

--- src/com/study/method/myreflect/proxyTest.java
+++ src/com/study/method/myreflect/proxyTest.java
@@ -30,13 +30,13 @@ public class proxyTest implements InvocationHandler {
         //通过反射获得操作者的start方法
         Method start = clazz.getDeclaredMethod("start",new Class[]{Method.class});
         //反射执行start方法
-        start.invoke(this.proxy,new Object[]{this.proxy.getClass()});
+        start.invoke(this.proxy,new Object[]{method});
         //执行要处理对象的原方法
         method.invoke(this.target,args);
         //通过反射获得end方法
         Method end = clazz.getDeclaredMethod("end",new Class[]{Method.class});
         //反射执行end方法
-        end.invoke(this.proxy,new Object[]{this.proxy.getClass()});
+        end.invoke(this.proxy,new Object[]{method});
         return result;
     }
 
